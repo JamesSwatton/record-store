1580aafafa6f9e6c612ee90aeed9413832d9e48c
James Swatton <jamessswatton@gmail.com>
2019-04-26 23:29:04 +0100

    add layout and new artist erb

--- artist_controller.rb
+++ artist_controller.rb
@@ -10,3 +10,12 @@ get '/artists' do
   @artists = Artist.sort_all()
   erb(:"artists/index")
 end
+
+get '/artists/new' do
+  erb(:"artists/new")
+end
+
+post '/artists' do
+  Artist.new(params).save()
+  redirect to '/artists'
+end
