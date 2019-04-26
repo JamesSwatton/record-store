f2567ef353da6fa17f457d8341f76a7c926e1409
James Swatton <jamessswatton@gmail.com>
2019-04-26 18:13:16 +0100

    add logic to post request '/albums' in controller

--- controller.rb
+++ controller.rb
@@ -21,10 +21,13 @@ post '/albums' do
   all_artists = Artist.all()
   artist_names = all_artists.map { |artist| artist.name }
 
-  if artist_names.include?(param['name'])
-      # use find_id_by_name method 
+  if artist_names.include?(params['name'])
+      # use find_id_by_name method
+      artist_id = Artist.find_id_by_name(params['name'])
+      Album.new( {'title' => params['title'], 'artist_id' => artist_id, 'quantity' => params['quantity'] }).save()
+      redirect to '/albums'
   end
 
-  Album.new( {'title' => params['title'], 'artist_id' => artist.id, 'quantity' => params['quantity'] }).save()
-  redirect to '/albums'
+  # Album.new( {'title' => params['title'], 'artist_id' => artist.id, 'quantity' => params['quantity'] }).save()
+  # redirect to '/albums'
 end
