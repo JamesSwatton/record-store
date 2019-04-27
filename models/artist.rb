430b423e54828cae5caf44a609061a4cfb2c7ff7
James Swatton <jamessswatton@gmail.com>
2019-04-27 15:20:08 +0100

    add delte class method to Artist class

--- models/artist.rb
+++ models/artist.rb
@@ -57,6 +57,13 @@ class Artist
     return results.first['id'].to_i
   end
 
+  def self.delete(id)
+    sql = "DELETE FROM artists
+          WHERE id = $1"
+    values = [id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.delete_all()
     sql = "DELETE FROM artists;"
     SqlRunner.run(sql)
