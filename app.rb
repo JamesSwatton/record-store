ecb5c6f53ff55f4c21206ea7211c54a694711b61
JamesSwatton <jamessswatton@gmail.com>
2019-08-14 20:27:52 +0100

    fixed path for '/'

--- app.rb
+++ app.rb
@@ -5,5 +5,8 @@ require_relative( './controllers/albums_controller.rb' )
 require_relative( './controllers/inventory_controller.rb' )
 
 get '/' do
+  @stock = Album.sort_all_by_artist_name()
+  @stock_levels = ["HIGH", "LOW"]
+  @total = Album.all_stock_total()
   erb(:"inventory/index")
 end
